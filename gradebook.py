c1b37f5686c34a3c43576c539ed1e4ade14f11bd
mandi628 <mandi628.codes@gmail.com>
2024-08-18 11:48:47 -0400

    First attempt gradeook

---
+++ gradebook.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+grades = {"":[]}
+
+def add_grade():
+    name = input("Enter student name: ")
+    grade = int(input("Enter grade to add: "))
+    # Lookup name amd add grade to list
+
+def add_student():
+    # Ask for student name
+    # Add student to dictionary with empty list
